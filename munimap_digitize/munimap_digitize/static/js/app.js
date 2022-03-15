942a554224880b73bff05be2ed02495c18086cc1
Simon Seyock <seyock@terrestris.de>
2022-03-15 13:57:52 +0100

    fix: load schema-form early

--- munimap_digitize/munimap_digitize/static/js/app.js
+++ munimap_digitize/munimap_digitize/static/js/app.js
@@ -22,6 +22,8 @@ require('./../../../../munimap/static/js/modules/slider.js');
 require('./../../../../munimap/static/js/modules/slider-directive.js');
 require('./../../../../munimap/static/js/modules/postmessage-service.js');
 
+require('angular-schema-form');
+require('angular-schema-form-bootstrap');
 
 require('./digitize-config.js');
 require('./digitize-controller.js');
