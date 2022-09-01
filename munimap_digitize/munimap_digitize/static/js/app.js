d5cb9cad4f3474c70c1fd0b50c4ea2b8683f35cf
jansule <suleiman@terrestris.de>
2022-09-01 15:11:33 +0200

    fix import order in digitize module

--- munimap_digitize/munimap_digitize/static/js/app.js
+++ munimap_digitize/munimap_digitize/static/js/app.js
@@ -6,6 +6,9 @@ proj4.defs('EPSG:25832', '+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0
 register(proj4);
 
 require('anol/src/anol/anol.js');
+require('angular-schema-form');
+require('angular-schema-form-bootstrap');
+
 require('./../../../../munimap/static/js/default-munimap-config.js');
 
 require('./../../../../munimap/static/js/base-config.js');
@@ -15,15 +18,12 @@ require('./../../../../munimap/static/js/settings-modal-controller.js');
 
 require('./../../../../munimap/static/js/modules/confirm.js');
 require('./../../../../munimap/static/js/modules/colorpicker.js');
-require('./../../../../munimap/static/js/modules/notifications.js');
 require('./../../../../munimap/static/js/modules/sidebar.js');
 require('./../../../../munimap/static/js/modules/servicebutton.js');
 require('./../../../../munimap/static/js/modules/slider.js');
 require('./../../../../munimap/static/js/modules/slider-directive.js');
+require('./../../../../munimap/static/js/modules/notifications.js');
 require('./../../../../munimap/static/js/modules/postmessage-service.js');
-
-require('angular-schema-form');
-require('angular-schema-form-bootstrap');
 
 require('./digitize-config.js');
 require('./digitize-controller.js');
